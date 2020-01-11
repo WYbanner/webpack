0a50ed49c3af89df1e467544fab5ee8a41d36ae0
wuyu <17635363134@163.com>
2020-01-11 18:12:46 +0800

    一期

---
+++ build/webpack.config.js
@@ -0,0 +1,17 @@
+// webpack.config.js
+const path = require('path')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development', //开发模式
+  entry: path.resolve(__dirname, '../src/main.js'), //入口文件
+  output: {
+    filename: '[name].[hash:8].js', //打包后的文件名称
+    path: path.resolve(__dirname, '../dist') // 打包后的文件
+  },
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: path.resolve(__dirname, '../public/index.html')
+  //   })
+  // ]
+}
